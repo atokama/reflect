45beb4fe1c7027d33bd78add80256199ea6bb15d
atoka <makrusan@gmail.com>
2020-04-18 19:36:25 +0300

    Test of serialization of compound struct

--- test/rttr_test.cpp
+++ test/rttr_test.cpp
@@ -2,24 +2,61 @@
 #include <gtest/gtest.h>
 #include <rttr/registration>
 
-struct Car {
-    std::string color;
-    int num_wheels;
-};
+using std::cout;
+using std::endl;
+using std::string;
+
+struct Car { string color; int num_wheels; };
+struct Traffic { Car car; int bandwidth; };
 
 RTTR_REGISTRATION {
      rttr::registration::class_<Car>("Car")
              .constructor<>()
              .property("color", &Car::color)
              .property("num_wheels", &Car::num_wheels);
+     rttr::registration::class_<Traffic>("Traffic")
+             .constructor<>()
+             .property("car", &Traffic::car)
+             .property("bandwidth", &Traffic::bandwidth);
+}
+
+Car car_obj{"yellow", 4};
+Traffic traffic_obj{car_obj, 100};
+
+//template <typename T>
+string serialize(rttr::variant obj) {//(const T& obj) {
+    std::stringstream ss{};
+    for (auto& prop : rttr::type::get(obj).get_properties()) {
+        ss << prop.get_name() << ": ";
+
+        auto type = prop.get_type();
+        if (type == rttr::type::get<int>()) {
+            ss << prop.get_value(obj).to_int();
+        } else if (type == rttr::type::get<string>()) {
+            ss << "\"" << prop.get_value(obj).to_string() << "\"";
+        } else {
+            ss << serialize(prop.get_value(obj));
+        }
+        ss << endl;
+    }
+    return ss.str();
 }
 
 TEST(rttr_test, 1) {
-    auto t = rttr::type::get<Car>();
-    for (auto& prop : t.get_properties()) {
-        std::cout << "name: " << prop.get_name() << std::endl;
-    }
-
-
+    string actual = serialize(traffic_obj);
+    string expected =
+            "car: color: \"yellow\"\n"
+            "num_wheels: 4\n"
+            "\n"
+            "bandwidth: 100\n";
+    ASSERT_EQ(actual, expected);
 }
 
+TEST(rttr_test, serialize_car) {
+    string actual = serialize(car_obj);
+    string expected =
+            "color: \"yellow\"\n"
+            "num_wheels: 4\n";
+
+    ASSERT_EQ(actual, expected);
+}
