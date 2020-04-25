384df1f27f751505ad67050d75b69cbece430ab0
atoka <makrusan@gmail.com>
2020-04-25 16:13:57 +0300

    Refactor duplicate code into deserialize_value()

--- include/reflect/deserialize.h
+++ include/reflect/deserialize.h
@@ -11,6 +11,21 @@ namespace reflect {
 void deserialize_object(const rapidjson::Value &json, rttr::variant &var);
 void deserialize_array(const rapidjson::Value &json, rttr::variant &var);
 
+void deserialize_value(const rapidjson::Value &json, rttr::variant &var) {
+    const auto t = var.get_type();
+    const auto type = t.is_wrapper() ? t.get_wrapped_type() : t;
+
+    if (type == rttr::type::get<int>()) {
+        var = json.GetInt();
+    } else if (type == rttr::type::get<std::string>()) {
+        var = std::string{json.GetString()};
+    } else if (type.is_sequential_container()) {
+        deserialize_array(json, var);
+    } else if (type.is_class()){
+        deserialize_object(json, var);
+    }
+}
+
 void deserialize_object(const rapidjson::Value &json, rttr::variant &var) {
     if (!json.IsObject()) {
         throw Error{""};
@@ -26,20 +41,9 @@ void deserialize_object(const rapidjson::Value &json, rttr::variant &var) {
         }
 
         const rapidjson::Value &json2 = json[name.data()];
-        const auto type2 = prop.get_type();
-        if (type2 == rttr::type::get<int>()) {
-            prop.set_value(var, json2.GetInt());
-        } else if (type2 == rttr::type::get<std::string>()) {
-            prop.set_value(var, std::string{json2.GetString()});
-        } else if (type2.is_sequential_container()) {
-            auto var2 = prop.get_value(var);
-            deserialize_array(json2, var2);
-            prop.set_value(var, var2);
-        } else if (type2.is_class()){
-            auto var2 = prop.get_value(var);
-            deserialize_object(json2, var2);
-            prop.set_value(var, var2);
-        }
+        auto var2 = prop.get_value(var);
+        deserialize_value(json2, var2);
+        prop.set_value(var, var2);
     }
 }
 
@@ -55,21 +59,10 @@ void deserialize_array(const rapidjson::Value &json, rttr::variant &var) {
     for (size_t i = 0; i != size; ++i) {
 
         const rapidjson::Value &json2 = json[i];
-        const auto type2 = arr.get_rank_type(1);
 
-        if (type2 == rttr::type::get<int>()) {
-            arr.set_value(i, json2.GetInt());
-        } else if (type2 == rttr::type::get<std::string>()) {
-            arr.set_value(i, std::string{json2.GetString()});
-        } else if (type2.is_sequential_container()) {
-            rttr::variant var2 = arr.get_value(i);
-            deserialize_array(json2, var2);
-            arr.set_value(i, var2);
-        } else if (type2.is_class()){
-            rttr::variant var2 = arr.get_value(i);
-            deserialize_object(json2, var2);
-            arr.set_value(i, var2);
-        }
+        auto var2 = arr.get_value(i);
+        deserialize_value(json2, var2);
+        arr.set_value(i, var2);
     }
 }
 
